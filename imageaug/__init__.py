ab1117a34ea93b0aac59e57095e35c9ce1581627
darwinharianto <darwinharianto@gmail.com>
2020-04-10 12:08:36 +0900

    updated setup.py and created test method

---
+++ imageaug/__init__.py
@@ -0,0 +1,2 @@
+def test_pip():
+    print("it works")
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='imageaug',
+    version='0.0.1',
+    description='Currently used as a wrapper for imgaug by aleju',
+    url='git@github.com:darwinharianto/imageaug-package.git',
+    author='Darwin Harianto',
+    author_email='hariantodarwin@gmail.com',
+    license='unlicense',
+    packages=['imageaug', 'imgaug'],
+    zip_safe=False
+)
