d16940daf2f48f6b60f322fae7c8bd042ccdd1bc
darwinharianto <darwinharianto@gmail.com>
2020-04-23 14:29:21 +0900

    sample mapper for detectron at test/augmented_loader.py

---
+++ test/augmented_loader.py
@@ -0,0 +1,136 @@
+import copy
+import torch
+import numpy as np
+from imgaug import augmenters as iaa
+import imgaug as ia
+from imgaug.augmentables.polys import PolygonsOnImage
+from imgaug.augmentables.kps import KeypointsOnImage
+from imgaug.augmentables.bbs import BoundingBoxesOnImage
+from detectron2.data import transforms as T
+from detectron2.data import detection_utils as utils
+from detectron2.structures import BoxMode
+import cv2
+
+from logger import logger
+from common_utils.cv_drawing_utils import cv_simple_image_viewer, draw_bbox, draw_keypoints, draw_segmentation
+from common_utils.common_types.keypoint import Keypoint2D_List, Keypoint2D
+from common_utils.common_types.bbox import BBox
+from common_utils.common_types.segmentation import Segmentation, Polygon
+from common_utils.file_utils import file_exists
+from imageaug import AugHandler, Augmenter as aug
+
+from aug_visualizer import aug_visualizer
+# from ..dataset_parser import Detectron2_Annotation_Dict
+
+
+def load_settings(handler_save_path: str):
+
+    handler_save_path = 'test_handler.json'
+    if not file_exists(handler_save_path):
+        handler = AugHandler(
+            [
+                aug.Crop(percent=[0.2, 0.5]),
+            ]
+        )
+        handler.save_to_path(save_path=handler_save_path, overwrite=True)
+        logger.info(f'Created new AugHandler save.')
+    else:
+        handler = AugHandler.load_from_path(handler_save_path)
+        handler.save_to_path(save_path=handler_save_path, overwrite=True)
+        logger.info(f'Loaded AugHandler from save.')
+
+    return handler
+
+def perform_augmentation(dataset_dict, handler: AugHandler):
+
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+
+    keypoints = []
+    bbox = []
+    segmentation = []
+    ann_instance = 0
+
+    for item in dataset_dict["annotations"]:
+
+        item["keypoints"] = Keypoint2D_List.from_numpy(np.array(item["keypoints"]))
+        item["bbox"] = BBox(xmin=item["bbox"][0], xmax=item["bbox"][0]+item["bbox"][2], ymin=item["bbox"][1], ymax=item["bbox"][1]+item["bbox"][3])
+        item["segmentation"] = Segmentation.from_list(item["segmentation"])
+
+        keypoints_num = len(item["keypoints"])
+        ann_instance += 1
+        keypoints.append(item["keypoints"])
+        bbox.append(item["bbox"])
+        segmentation.append(item["segmentation"])
+        item["bbox_mode"] = BoxMode.XYXY_ABS
+
+
+    image, keypoints, bbox, poly = handler(image=image, keypoints= keypoints, bounding_boxes=bbox, polygons=segmentation)
+    kpts_aug_list = keypoints[0].to_numpy(demarcation=True)[:, :2].reshape(ann_instance, keypoints_num, 2)
+    kpts_aug_list = [[[x, y, 2] for x, y in kpts_aug] for kpts_aug in kpts_aug_list]
+    keypoints = [Keypoint2D_List.from_list(kpts_aug, demarcation=True) for kpts_aug in kpts_aug_list]
+
+    annots = []
+    for i in range(len(dataset_dict["annotations"])):
+        item = dataset_dict["annotations"][i]
+        if "keypoints" in item:
+            item["keypoints"] = np.asarray(keypoints[i].to_list(), dtype="float64").reshape(-1,3)
+        if "bbox" in item:
+            item["bbox"] = bbox[i].to_list()
+            print(item["bbox"])
+        if "segmentation" in item:
+            item["segmentation"] = [poly[i].to_list()]
+        annots.append(item)
+        
+    del dataset_dict["annotations"]
+
+    return image, annots
+
+
+
+def mapper(dataset_dict):
+    # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
+    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+
+    handler = load_settings(handler_save_path = 'test_handler.json')
+
+
+    # img_buffer = image
+    # img_buffer.append(img)
+    
+    image, annots = perform_augmentation(dataset_dict=dataset_dict, handler=handler)
+
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+    instances = utils.annotations_to_instances(annots, image.shape[:2])
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+
+    # dataset_dict["instances"] = utils.filter_empty_instances(instances, by_box=True, by_mask=False)
+
+    if True:
+        vis_img = image.copy()
+        bbox_list = [BBox.from_list(vals) for vals in dataset_dict["instances"].gt_boxes.tensor.numpy().tolist()]
+        seg_list = [Segmentation([Polygon.from_list(poly.tolist(), demarcation=False) for poly in seg_polys]) for seg_polys in dataset_dict["instances"].gt_masks.polygons]
+        kpts_list = [Keypoint2D_List.from_numpy(arr, demarcation=True) for arr in dataset_dict["instances"].gt_keypoints.tensor.numpy()]
+        # print(bbox_list, seg_list, kpts_list)
+        for bbox, seg, kpts in zip(bbox_list, seg_list, kpts_list):
+            if len(seg) > 0 and False:
+                vis_img = draw_segmentation(img=vis_img, segmentation=seg, transparent=True)
+            vis_img = draw_bbox(img=vis_img, bbox=bbox)
+            vis_img = draw_keypoints(img=vis_img, keypoints=kpts.to_numpy(demarcation=True)[:, :2].tolist(), radius=1)
+        cv2.imshow("a", vis_img)
+        cv2.waitKey(5000)
+        # aug_visualizer.step(vis_img)
+
+    return dataset_dict
+
+
+
+if __name__ == "__main__":
+    import json
+    with open('/Users/darwinharianto/Desktop/hayashida/Unreal/18_03_2020_18_03_10_coco-data/HSR-detectron.json') as f:
+        dataset_dicts = json.loads(f.read())
+    for dataset_dict in dataset_dicts:
+        dataset_dict = mapper(dataset_dict)
+        break
+    # dataset_dict = mapper(dataset_dict)
+    # data_loader = build_detection_train_loader(cfg, mapper=mapper)
