8e3e3dc000b8f1970be3418b3db519d1179d5a95
darwinharianto <darwinharianto@gmail.com>
2020-05-15 15:06:06 +0900

    test bbox segmentation

--- imageaug/aug_mode.py
+++ imageaug/aug_mode.py
@@ -911,8 +911,7 @@ class AugHandler(BaseModeHandler['AugHandler', 'Any']):
 
             for item in dataset_dict["annotations"]:
                 ann_instance += 1
-
-
+                
                 if "keypoints" in item:
                     if len(item["keypoints"]) != 0:
                         item["keypoints"] = Keypoint2D_List.from_numpy(np.array(item["keypoints"]))
@@ -938,6 +937,10 @@ class AugHandler(BaseModeHandler['AugHandler', 'Any']):
                         raise TypeError("invalid annotation number from bbox and segmentation")
                 
             
+            logger.red("before augmentation: annot, bbox")
+            print(f"segmentation number: {len(segmentation)}")
+            print(len(dataset_dict["annotations"]))
+            print(len(bbox))
             if len(keypoints) != 0 and len(bbox) != 0 and len(segmentation) != 0:
                 image, keypoints, bbox, poly = self.perform_aug_modes(image=image, keypoints= keypoints, bounding_boxes=bbox, polygons=segmentation)
             elif len(keypoints) != 0 and len(bbox) != 0 and len(segmentation) == 0:
@@ -958,6 +961,11 @@ class AugHandler(BaseModeHandler['AugHandler', 'Any']):
                 kpts_aug_list = [[[x, y, 2] for x, y in kpts_aug] for kpts_aug in kpts_aug_list]
                 keypoints = [Keypoint2D_List.from_list(kpts_aug, demarcation=True) for kpts_aug in kpts_aug_list]
 
+            logger.red("after augmentation: annot, bbox count")
+            if "poly" in locals():
+                print(f"segmentation number: {len(poly)}")
+            print(len(dataset_dict["annotations"]))
+            print(len(bbox))
             for i in range(len(dataset_dict["annotations"])):
                 if "keypoints" in dataset_dict["annotations"][i]:
                     if len(dataset_dict["annotations"][i]["keypoints"]) != 0:
