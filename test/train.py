b15e7fcb5e68a0bb203141779d573deba53f89c2
darwinharianto <44696192+darwinharianto@users.noreply.github.com>
2020-05-19 13:05:54 +0900

    Update train.py

--- test/train.py
+++ test/train.py
@@ -6,6 +6,7 @@ from annotation_utils.dataset.config.dataset_config import \
 from pasonatron.detectron2.util.dataset_parser import Detectron2_Annotation_Dict
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.datasets import register_coco_instances
+from detectron2.data.dataset_mapper import DatasetMapper
 import cv2
 import os
 from tqdm import tqdm
@@ -19,9 +20,7 @@ from annotation_utils.coco.structs import COCO_Dataset
 from common_utils.file_utils import file_exists
 
 from pasonatron.detectron2.lib.roi_heads import CustomROIHeads, ROI_HEADS_REGISTRY
-# from pasonatron.detectron2.lib.trainer import COCO_Keypoint_Trainer
 
-# from augmented_loader import mapper # Change this import path
 import imageaug
 from imageaug import AugHandler, Augmenter as aug
 from detectron2.data import build_detection_train_loader
@@ -67,79 +66,97 @@ def load_augmentation_settings(handler_save_path: str):
 
     return handler
 
-def mapper(dataset_dict):
-    # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
-    dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-    image = utils.read_image(dataset_dict["file_name"], format="BGR")
-
-    handler = load_augmentation_settings(handler_save_path = 'augmentation_settings.json')  
-
-    ############################################################################################
-    ################################ this process need more review #############################
-    ############################################################################################
-    for i in range(len(dataset_dict["annotations"])):
-        item = dataset_dict["annotations"][i]
-        if "segmentation" in item:
-            if len(item["segmentation"]) != 0:
-                for j in range(len(item["segmentation"])):
-                    segmentation_list = item["segmentation"][j]
-                    if len(segmentation_list) < 5:
-                        logger.red(f"segmentation has abnormal point count {segmentation_list}" )
-                        continue 
-                    segmentation_list = np.array(segmentation_list).reshape(-1,2)
-                    poly_shapely = Polygon(segmentation_list)
-                    poly_list = list(zip(*poly_shapely.exterior.coords.xy))
-                    line_non_simple = shapely.geometry.LineString(poly_list)
-                    mls = shapely.ops.unary_union(line_non_simple)
-                    polygons = list(shapely.ops.polygonize(mls))
-                    if len(polygons) == 0:
-                        item["segmentation"][j] = segmentation_list
-                    else:
-                        shapely_polygon = Polygon(polygons[0])
-                        vals_tuple = shapely_polygon.exterior.coords.xy
-                        numpy_array = np.array(vals_tuple).T[:-1]
-                        flattened_list = numpy_array.reshape(-1).tolist()
-                        item["segmentation"][j] = flattened_list
-                dataset_dict["annotations"][i]["segmentation"] = item["segmentation"]
-    ##############################################################################################
-
-    seg_count = 0
-    bbox_count = 0
-    for item in dataset_dict["annotations"]:
-        if "segmentation" in item:
-            seg_count +=1
-        if "bbox" in item:
-            bbox_count +=1
-        if bbox_count != seg_count:
-            logger.red(f"bbox and seg = {seg_count}, {bbox_count}")
-            print(dataset_dict["file_name"])
-
-            
-
-
-    image, dataset_dict = handler(image=image, dataset_dict_detectron=dataset_dict)
+class MyMapper(DatasetMapper):
+    def __init__(self, cfg, json_file_path, is_train=True):
+        super().__init__(cfg, is_train=is_train)
+        self.aug_handler = AugHandler.load_from_path(json_file_path)
     
-    annots = [obj for obj in dataset_dict.pop("annotations")
-		if obj.get("iscrowd", 0) == 0]
+    def __call__(self, dataset_dict):
+        """
+        Args:
+            dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
+        Returns:
+            dict: a format that builtin models in detectron2 accept
+        """
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        # USER: Write your own image loading if it's not from a file
+        image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
+        utils.check_image_size(dataset_dict, image)
 
-    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
-    instances = utils.annotations_to_instances(annots, image.shape[:2])
-    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+        ### my code ##
+        image, dataset_dict = self.aug_handler(image=image, dataset_dict_detectron=dataset_dict)
+        ### my code ##
 
-    if True:
-        vis_img = image.copy()
-        bbox_list = [BBox.from_list(vals) for vals in dataset_dict["instances"].gt_boxes.tensor.numpy().tolist()]
-        seg_list = [Segmentation([Polygon.from_list(poly.tolist(), demarcation=False) for poly in seg_polys]) for seg_polys in dataset_dict["instances"].gt_masks.polygons]
-        for bbox, seg in zip(bbox_list, seg_list):
-            if len(seg) > 0 and False:
-                vis_img = draw_segmentation(img=vis_img, segmentation=seg, transparent=True)
-            vis_img = draw_bbox(img=vis_img, bbox=bbox)
-        aug_visualizer.step(vis_img)
+        if "annotations" not in dataset_dict:
+            image, transforms = T.apply_transform_gens(
+                ([self.crop_gen] if self.crop_gen else []) + self.tfm_gens, image
+            )
+        else:
+            # Crop around an instance if there are instances in the image.
+            # USER: Remove if you don't use cropping
+            if self.crop_gen:
+                crop_tfm = utils.gen_crop_transform_with_instance(
+                    self.crop_gen.get_crop_size(image.shape[:2]),
+                    image.shape[:2],
+                    np.random.choice(dataset_dict["annotations"]),
+                )
+                image = crop_tfm.apply_image(image)
+            image, transforms = T.apply_transform_gens(self.tfm_gens, image)
+            if self.crop_gen:
+                transforms = crop_tfm + transforms
 
-    return dataset_dict
+        image_shape = image.shape[:2]  # h, w
 
+        # Pytorch's dataloader is efficient on torch.Tensor due to shared-memory,
+        # but not efficient on large generic data structures due to the use of pickle & mp.Queue.
+        # Therefore it's important to use torch.Tensor.
+        dataset_dict["image"] = torch.as_tensor(np.ascontiguousarray(image.transpose(2, 0, 1)))
 
-###### test end
+        # USER: Remove if you don't use pre-computed proposals.
+        if self.load_proposals:
+            utils.transform_proposals(
+                dataset_dict, image_shape, transforms, self.min_box_side_len, self.proposal_topk
+            )
+
+        if not self.is_train:
+            # USER: Modify this if you want to keep them for some reason.
+            dataset_dict.pop("annotations", None)
+            dataset_dict.pop("sem_seg_file_name", None)
+            return dataset_dict
+
+        if "annotations" in dataset_dict:
+            # USER: Modify this if you want to keep them for some reason.
+            for anno in dataset_dict["annotations"]:
+                if not self.mask_on:
+                    anno.pop("segmentation", None)
+                if not self.keypoint_on:
+                    anno.pop("keypoints", None)
+
+            # USER: Implement additional transformations if you have other types of data
+            annos = [
+                utils.transform_instance_annotations(
+                    obj, transforms, image_shape, keypoint_hflip_indices=self.keypoint_hflip_indices
+                )
+                for obj in dataset_dict.pop("annotations")
+                if obj.get("iscrowd", 0) == 0
+            ]
+            instances = utils.annotations_to_instances(
+                annos, image_shape, mask_format=self.mask_format
+            )
+            # Create a tight bounding box from masks, useful when image is cropped
+            if self.crop_gen and instances.has("gt_masks"):
+                instances.gt_boxes = instances.gt_masks.get_bounding_boxes()
+            dataset_dict["instances"] = utils.filter_empty_instances(instances)
+
+        # USER: Remove if you don't do semantic/panoptic segmentation.
+        if "sem_seg_file_name" in dataset_dict:
+            with PathManager.open(dataset_dict.pop("sem_seg_file_name"), "rb") as f:
+                sem_seg_gt = Image.open(f)
+                sem_seg_gt = np.asarray(sem_seg_gt, dtype="uint8")
+            sem_seg_gt = transforms.apply_segmentation(sem_seg_gt)
+            sem_seg_gt = torch.as_tensor(sem_seg_gt.astype("long"))
+            dataset_dict["sem_seg"] = sem_seg_gt
+        return dataset_dict
 
 def make_config_file_from_datasets(datasets_dict: {}, config_path: str):
     handler = DatasetConfigCollectionHandler()
@@ -178,7 +195,6 @@ def combine_dataset_from_config_file(config_path: str, dest_folder_img: str, des
         show_pbar=True
     )
     combined_dataset.save_to_path(save_path=dest_json_file, overwrite=True)
-    # combined_dataset.display_preview(kpt_idx_offset=-1, start_idx=0)
 
 def register_dataset_to_detectron(instance_name: str,img_dir_path: str, ann_path: str):
     register_coco_instances(
@@ -188,47 +204,8 @@ def register_dataset_to_detectron(instance_name: str,img_dir_path: str, ann_path
         image_root=img_dir_path
     )
     MetadataCatalog.get(instance_name).thing_classes = ['measure']
-    # MetadataCatalog.get(instance_name).keypoint_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
-    # MetadataCatalog.get(instance_name).keypoint_flip_map = []
-    # MetadataCatalog.get(instance_name).keypoint_connection_rules = [
-    #     ('A', 'B', (0, 0, 255)),
-    #     ('B', 'C', (0, 0, 255)),
-    #     ('C', 'D', (0, 0, 255)),
-    #     ('D', 'A', (0, 0, 255)),
-    #     ('A', 'E', (0, 0, 255)),
-    #     ('B', 'F', (0, 0, 255)),
-    #     ('C', 'G', (0, 0, 255)),
-    #     ('D', 'H', (0, 0, 255)),
-    #     ('E', 'F', (0, 0, 255)),
-    #     ('F', 'G', (0, 0, 255)),
-    #     ('G', 'H', (0, 0, 255)),
-    #     ('H', 'E', (0, 0, 255)),
-    #     ('E', 'I', (0, 0, 255)),
-    #     ('F', 'J', (0, 0, 255)),
-    #     ('G', 'K', (0, 0, 255)),
-    #     ('H', 'L', (0, 0, 255)),
-    #     ('I', 'J', (0, 0, 255)),
-    #     ('J', 'K', (0, 0, 255)),
-    #     ('K', 'L', (0, 0, 255)),
-    #     ('L', 'I', (0, 0, 255))
-    # ]
 
 def setup_config_file(instance_name:str, cfg):
-
-    # # change model zoo  and weightsto use other NN type
-    # cfg.merge_from_file(model_zoo.get_config_file("COCO-Keypoints/keypoint_rcnn_R_50_FPN_1x.yaml"))
-    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
-    # cfg.MODEL.ROI_HEADS.NAME = 'CustomROIHeads'
-    # cfg.DATASETS.TRAIN = (instance_name,)
-    # cfg.DATALOADER.NUM_WORKERS = 2
-    # cfg.SOLVER.IMS_PER_BATCH = 2
-    # cfg.SOLVER.BASE_LR = 0.003
-    # cfg.SOLVER.MAX_ITER = (10000)
-    # cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = (512)   # faster, and good enough for this toy dataset (default: 512)
-    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class
-    # cfg.MODEL.ROI_KEYPOINT_HEAD.NUM_KEYPOINTS = 12
-    # cfg.INPUT.MIN_SIZE_TRAIN = 512
-
 
     # change model zoo  and weightsto use other NN type
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_50_FPN_1x.yaml"))
@@ -240,9 +217,7 @@ def setup_config_file(instance_name:str, cfg):
     cfg.SOLVER.BASE_LR = 0.003
     cfg.SOLVER.MAX_ITER = (10000)
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = (512)   # faster, and good enough for this toy dataset (default: 512)
-    # cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class
     cfg.MODEL.RETINANET.NUM_CLASSES = 1
-    # cfg.MODEL.ROI_KEYPOINT_HEAD.NUM_KEYPOINTS = 12
     cfg.INPUT.MIN_SIZE_TRAIN = 512
 
     return cfg
@@ -253,22 +228,15 @@ class Trainer(DefaultTrainer):
 
     @classmethod
     def build_train_loader(cls, cfg):
+	mapper = MyMapper()
         return build_detection_train_loader(cfg, mapper=mapper)
 
 if __name__ == "__main__":
 
     instance_name = "measure"
-    # config_path = "scratch_config.yaml"
     dest_folder_img_combined = "/home/doors/workspace/darwin/from_gosar/my_real_measure_selected/img/mp_900_26_04_2020_22_18_37_coco-data"
     dest_json_file_combined = "/home/doors/workspace/darwin/from_gosar/my_real_measure_selected/img/mp_900_26_04_2020_22_18_37_coco-data/measure-only.json"
 
-    # datasets_dict_img_annot = {
-    #     './dummy_data/18_03_2020_18_03_10_coco-data' : './dummy_data/18_03_2020_18_03_10_coco-data/HSR-coco.json',
-    #     './dummy_data/31_03_2020_17_37_04_coco-data': './dummy_data/31_03_2020_17_37_04_coco-data/HSR-coco.json'
-    # }
-
-    # make_config_file_from_datasets(datasets_dict= datasets_dict_img_annot, config_path= config_path)
-    # combine_dataset_from_config_file(config_path= config_path, dest_folder_img= "./combined_img", dest_json_file= "./combined.json" )
     register_dataset_to_detectron(instance_name=instance_name,img_dir_path= dest_folder_img_combined, ann_path = dest_json_file_combined)
 
     cfg = setup_config_file(instance_name=instance_name, cfg=get_cfg())
@@ -279,5 +247,4 @@ if __name__ == "__main__":
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=False)
     trainer.train()
-    # trainer.resume_or_load(resume=True)
 
